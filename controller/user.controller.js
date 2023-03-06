563144c5a1cfca2d32a3069fc90928b78789145f
Viro <virokemin@gmail.com>
2023-03-06 12:27:30 +0530

    UserComplete

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -2,9 +2,9 @@ const UserService = require('../services/user.services');
 
 exports.register = async(req,res,next)=>{
     try{
-        const {email,password} = req.body;
+        const {username,fullname,email,password} = req.body;
 
-        const successRes = await UserService.registerUser(email,password);
+        const successRes = await UserService.registerUser(username,fullname,email,password);
 
         res.json({status:true,sucess:"User Registered Successfully"})
 
@@ -16,9 +16,9 @@ exports.register = async(req,res,next)=>{
 }
 exports.login = async(req,res,next)=>{
     try{
-        const {email,password} = req.body;
+        const {username,password} = req.body;
 
-        const user = await UserService.checkuser(email);
+        const user = await UserService.checkuser(username);
 
         if(!user){
             throw new Error('User Dont Exist');
@@ -28,7 +28,7 @@ exports.login = async(req,res,next)=>{
         if(isMatch===false){
             throw new Error('Password Invalid');
         }
-        let tokenData = {_id:user._id,email:user.email};
+        let tokenData = {_id:user._id,username:user.username};
 
         const token = await UserService.generateToken(tokenData,"secretKey",'1h');
 
--- services/user.services.js
+++ services/user.services.js
@@ -2,18 +2,18 @@ const UserModel = require('../model/user.model');
 const jwt = require('jsonwebtoken');
 
 class UserService{
-    static async registerUser(email,password){
+    static async registerUser(username,fullname,email,password){
         try{
-            const createUser = new UserModel({email,password});
+            const createUser = new UserModel({username,fullname,email,password});
             return await createUser.save();
 
         }catch(err){
             throw err;
         }
     }
-    static async checkuser(email){
+    static async checkuser(username){
         try {
-            return await UserModel.findOne({email});
+            return await UserModel.findOne({username});
         } catch (error) {
             throw error;
         }
