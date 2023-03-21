eba45a6b2819aa30ebad2b6cc09387ebfb616fb6
manoj <manojlakshan9991@gmail.com>
2023-03-21 11:40:56 -0700

    messageModel and create routes

--- routers/user.router.js
+++ routers/user.router.js
@@ -1,9 +1,11 @@
-
+const MessageController = require("../controller/message.controllers");
 const UserController = require('../controller/user.controller');
 
 const router = require('express').Router();
 
 router.post('/registration',UserController.register);
 router.post('/login',UserController.login);
+router.get('/users',UserController.showusers);
+router.post('/message',MessageController.messages);//message routes
 
 module.exports=router;
