dbaed62ad3c0c398d5890d3d886f3ba3d614eec5
Viro <virokemin@gmail.com>
2023-05-23 00:24:10 +0530

    cartFinal

--- model/cart.model.js
+++ model/cart.model.js
@@ -1,38 +1,40 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
+const db = require('../config/db');
 
-const cart = mongoose.model(
-    "bucket",
-    mongoose.Schema({
-        username:{
-            type: String,
-            required: true
+const { Schema } = mongoose;
+
+
+const cartSchema = new Schema(
+    {
+      username: {
+        type: String,
+        required: true,
+      },
+      items: [
+        {
+          item: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "item",
+            required: true,
+          },
+          qty: {
+            type: Number,
+            required: true,
+          },
         },
-        items:[
-            {
-                item:{
-                    type:mongoose.Schema.Types.ObjectId,
-                    ref:"item",
-                    require:true,
-                },
-                qty:{
-                    type:Number,
-                    required:true
-                }
-            }
-        ]
-    },{
-        toJSON: {
-            tranform: function(model,ret){
-                ret.cartId = ret._id.toString();
-                delete ret._id;
-                delete ret.__v;
-            }
-        }
-    },{
-        timestamps: true 
-    })
-);
+      ],
+    },
+    {
+      toJSON: {
+        transform: function (model, ret) {
+          ret.cartId = ret._id.toString();
+          delete ret._id;
+          delete ret.__v;
+        },
+      },
+      timestamps: true,
+    }
+  );
 
-module.exports = {
-    cart
-}
+const cart = db.model('cart',cartSchema);
+module.exports = cart;
